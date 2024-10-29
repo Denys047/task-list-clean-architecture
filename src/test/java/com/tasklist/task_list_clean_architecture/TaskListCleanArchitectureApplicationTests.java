b0d0d1deae62f1ba9a63ec863ebe7bc616fff79b
Denys <dendet94@gmail.com>
2024-10-30 02:26:05 +0300

    init-commit

---
+++ src/test/java/com/tasklist/task_list_clean_architecture/TaskListCleanArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tasklist.task_list_clean_architecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskListCleanArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
